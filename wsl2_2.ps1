b3bead7eb07435e1e0acef06eb22df58db09bdc1
Tomoyuki YaMaNashi <ae86bg5@yahoo.co.jp>
2023-05-13 07:28:21 +0900

    Update wsl2_2.ps1

--- wsl2_2.ps1
+++ wsl2_2.ps1
@@ -2,5 +2,5 @@ wsl --set-default-version 2
 wget https://wslstorestorage.blob.core.windows.net/wslblob/wsl_update_x64.msi -OutFile wsl_update_x64.msi
 wsl_update_x64.msi /q
 pause
-winget install 9P9TQF7MRM4R --accept-package-agreements
-winget install 9PN20MSR04DW --accept-package-agreements
+winget install --name "Windows Subsystem for Linux"
+winget install --name "Ubunut 22.04.2 LTS"
