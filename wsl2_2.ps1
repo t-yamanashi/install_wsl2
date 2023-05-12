6068278d05596fde8c707e894b5a706b5a762655
Tomoyuki YaMaNashi <ae86bg5@yahoo.co.jp>
2023-05-13 07:31:27 +0900

    Update wsl2_2.ps1

--- wsl2_2.ps1
+++ wsl2_2.ps1
@@ -3,4 +3,4 @@ wget https://wslstorestorage.blob.core.windows.net/wslblob/wsl_update_x64.msi -O
 wsl_update_x64.msi /q
 pause
 winget install --name "Windows Subsystem for Linux"
-winget install --name "Ubunut 22.04.2 LTS"
+winget install --name "Ubuntu 22.04.2 LTS"
