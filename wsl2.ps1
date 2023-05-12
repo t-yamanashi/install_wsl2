06cf3e86a0793a3a6c7ac137f6781876563e9b6e
Tomoyuki YaMaNashi <ae86bg5@yahoo.co.jp>
2023-05-12 22:35:49 +0900

    Update wsl2.ps1

--- wsl2.ps1
+++ wsl2.ps1
@@ -1,2 +1,3 @@
 dism.exe /online /enable-feature /featurename:Microsoft-Windows-Subsystem-Linux /all /norestart
 dism.exe /online /enable-feature /featurename:VirtualMachinePlatform /all
+echo wsl2installreboot > rebootok.txt
