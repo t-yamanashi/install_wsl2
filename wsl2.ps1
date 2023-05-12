41998b60f0132ad2e4c60401511f9229e3bc3961
Tomoyuki YaMaNashi <ae86bg5@yahoo.co.jp>
2023-05-12 23:28:34 +0900

    Update wsl2.ps1

--- wsl2.ps1
+++ wsl2.ps1
@@ -2,3 +2,5 @@ echo Install_WSL_and_VirtualMachinePlatform
 dism.exe /online /enable-feature /featurename:Microsoft-Windows-Subsystem-Linux /all /norestart
 dism.exe /online /enable-feature /featurename:VirtualMachinePlatform /all
 echo "echo Download" > wsl2.ps1
+
+echo "rm wsl2.ps1" >> wsl2.ps1
