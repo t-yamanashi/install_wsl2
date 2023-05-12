8404834c618f3ff2b3c9bff67fe1a42ccd90ea67
Tomoyuki YaMaNashi <ae86bg5@yahoo.co.jp>
2023-05-12 23:24:59 +0900

    Update wsl2.ps1

--- wsl2.ps1
+++ wsl2.ps1
@@ -1,10 +1,4 @@
-if( Test-Path rebootok.txt) {
-  echo ----------------
-} else {
-  echo Install_WSL_and_VirtualMachinePlatform
-  dism.exe /online /enable-feature /featurename:Microsoft-Windows-Subsystem-Linux /all /norestart
-  dism.exe /online /enable-feature /featurename:VirtualMachinePlatform /all
-  echo wsl2installreboot > rebootok.txt
-}
-
-echo Download
+echo Install_WSL_and_VirtualMachinePlatform
+dism.exe /online /enable-feature /featurename:Microsoft-Windows-Subsystem-Linux /all /norestart
+dism.exe /online /enable-feature /featurename:VirtualMachinePlatform /all
+echo "echo Download" > wsl2.ps1
