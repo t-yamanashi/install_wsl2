e464bfc3dcc0793028a130e7e5e7390222f8575d
Tomoyuki YaMaNashi <ae86bg5@yahoo.co.jp>
2023-05-12 23:33:16 +0900

    Update wsl2.ps1

--- wsl2.ps1
+++ wsl2.ps1
@@ -2,5 +2,8 @@ echo Install_WSL_and_VirtualMachinePlatform
 dism.exe /online /enable-feature /featurename:Microsoft-Windows-Subsystem-Linux /all /norestart
 dism.exe /online /enable-feature /featurename:VirtualMachinePlatform /all
 echo "echo Download" > wsl2.ps1
-
+echo "wsl --set-default-version 2" >> wsl2.ps1
+echo "wget https://wslstorestorage.blob.core.windows.net/wslblob/wsl_update_x64.msi -OutFile wsl_update_x64.msi" >> wsl2.ps1
+echo "start wsl_update_x64.msi"
+echo "echo ok" >> wsl2.ps1
 echo "rm wsl2.ps1" >> wsl2.ps1
