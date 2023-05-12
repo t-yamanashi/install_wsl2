dcbd14b6bebd6d49164ad6e8df094929e961b769
Tomoyuki YaMaNashi <ae86bg5@yahoo.co.jp>
2023-05-13 00:47:39 +0900

    Update wsl2_2.ps1

--- wsl2_2.ps1
+++ wsl2_2.ps1
@@ -1,4 +1,6 @@
-wsl --set-default-version 2"
+wsl --set-default-version 2
 wget https://wslstorestorage.blob.core.windows.net/wslblob/wsl_update_x64.msi -OutFile wsl_update_x64.msi
 wsl_update_x64.msi /qa
-
+pause
+winget install 9P9TQF7MRM4R　--accept-package-agreements
+winget install 9PN20MSR04DW　--accept-package-agreements
