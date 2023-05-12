b4c841ccf17565611cfdad6c304be44d4e002c89
Tomoyuki YaMaNashi <ae86bg5@yahoo.co.jp>
2023-05-12 22:48:32 +0900

    Update wsl2.ps1

--- wsl2.ps1
+++ wsl2.ps1
@@ -1,11 +1,10 @@
 if (Test-Path .¥rebootok.txt) {
-  echo 再起動後の処理を実行します
+  echo ----------------
 } else {
-  echo 再起動の案内が表示後yを入力して再起動してください
-  echo 再起動後に再度 wsl2.ps1を実行してください
+  echo Install_WSL_and_VirtualMachinePlatform
   dism.exe /online /enable-feature /featurename:Microsoft-Windows-Subsystem-Linux /all /norestart
   dism.exe /online /enable-feature /featurename:VirtualMachinePlatform /all
   echo wsl2installreboot > rebootok.txt
 }
 
-echo 続いてダウンロード
+echo Download
